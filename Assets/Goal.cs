2fbc9f245f2d82cc90e730159a995e233a128587
tombonanni <tombonanni@gmail.com>
2016-02-29 02:45:01 -0500

    Added files via upload

---
+++ Assets/Goal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+    static public bool goalMet = false;
+	
+	void OnTriggerEnter ( Collider other ) {
+	
+        if(other.gameObject.tag == "Projectile")
+        {
+
+            Goal.goalMet = true;
+
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = 0.9f;
+            GetComponent<Renderer>().material.color = c;
+
+        }    
+	}
+}
